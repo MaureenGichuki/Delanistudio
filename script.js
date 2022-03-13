eeb0d8673f871bf0a83f9e9b2063754564f282c6
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-03-14 00:19:34 +0300

    "Adds interactivity to the what we do section"

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('#icon').click(function(){
+        $('#m61').toggle();
+	});
+    $('#icon1').click(function(){
+        $('#m62').toggle();
+    });
+    $('#icon2').click(function(){
+        $('#m63').toggle();
+    });
+
+});
