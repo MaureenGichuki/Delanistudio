ef565b4b6fefc6c54a166deb056e2a7673cfaa93
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-03-14 05:50:14 +0300

    "Adds alert for the message in contact us section"

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
     $('#icon2').click(function(){
         $('#m63').toggle();
     });
-
 });
+    function myFunction(name,email,text){
+    var name = $('#nm1').val();
+    var email = $('#em1').val();
+    var text = $('#mm1').val();
+
+    if(name == ''|| email== '' || text == ''){
+        alert("Invalid please input the correct values on the form !");
+    }
+    else(
+        alert( "Hey" + " " + name + " " + "we have received your message. Thank you for reaching out to us")
+        
+    );
+    };
